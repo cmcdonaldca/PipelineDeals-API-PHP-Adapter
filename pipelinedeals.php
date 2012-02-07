f2879a50e486960df4516bbb704e406a835f2f39
Shopify Apps <shapps@shapps.ca>
2012-02-06 20:32:15 -0800

    Removed debug code

--- pipelinedeals.php
+++ pipelinedeals.php
@@ -44,7 +44,6 @@ class PipelineDealsClient {
 	private function curlSetopts($ch, $method, $payload, $request_headers)
 	{
 		curl_setopt($ch, CURLOPT_HEADER, true);
-		curl_setopt($ch, CURLOPT_VERBOSE, true);
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
 		curl_setopt($ch, CURLOPT_MAXREDIRS, 3);
